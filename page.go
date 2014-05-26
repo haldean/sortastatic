f7f70bd12ed2b6dba8de47021b3986005d19a6b4
Will Haldean Brown <will.h.brown@gmail.com>
2014-05-26 16:01:27 -0700

    Sort pages case-insensitively.

--- page.go
+++ page.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/haldean/gommd"
 )
@@ -134,6 +135,8 @@ func (p *Page) Load() error {
 
 type ByTitle []*Page
 
-func (a ByTitle) Len() int           { return len(a) }
-func (a ByTitle) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
-func (a ByTitle) Less(i, j int) bool { return a[i].Title < a[j].Title }
+func (a ByTitle) Len() int      { return len(a) }
+func (a ByTitle) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a ByTitle) Less(i, j int) bool {
+	return strings.ToLower(a[i].Title) < strings.ToLower(a[j].Title)
+}
