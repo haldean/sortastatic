53de58cf5c5e4858dcb44432f7c6a9281f41e907
Will Haldean Brown <will.h.brown@gmail.com>
2014-05-27 08:32:39 -0700

    Clean up flags

--- main.go
+++ main.go
@@ -4,15 +4,15 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"path"
 	"text/template"
 )
 
 var index = flag.String("index", "", "location of markdown files")
-var itemplate = flag.String("index-template", "", "location of index template")
-var nftemplate = flag.String("404-template", "", "location of 404 page template")
-var ptemplate = flag.String("page-template", "", "location of page template")
-var commondir = flag.String("common-dir", "", "location of files shared between pages")
+var templates = flag.String("templates", "", "location of page template")
+var static = flag.String("static", "", "location of files shared between pages")
 var reload = flag.Bool("reload", false, "reload a running instance of sortastatic")
+var bindAddr = flag.String("bind", "127.0.0.1:8080", "address/port to bind to")
 
 var pages map[string]*Page
 var sorted []*Page
@@ -21,18 +21,22 @@ var pageTemplate *template.Template
 var notFoundTemplate *template.Template
 
 func LoadTemplates() {
+	var tpath = func(template string) string {
+		return path.Join(*templates, template + ".html")
+	}
+
 	var err error
-	indexTemplate, err = template.ParseFiles(*itemplate)
+	indexTemplate, err = template.ParseFiles(tpath("index"))
 	if err != nil {
 		log.Fatalf("could not load index template: %v", err)
 	}
-	notFoundTemplate, err = template.ParseFiles(*nftemplate)
-	if err != nil {
-		log.Printf("warning: could not load 404 template: %v", err)
-	}
-	pageTemplate, err = template.ParseFiles(*ptemplate)
+	pageTemplate, err = template.ParseFiles(tpath("page"))
 	if err != nil {
 		log.Fatalf("could not load page template: %v", err)
+	}
+	notFoundTemplate, err = template.ParseFiles(tpath("404"))
+	if err != nil {
+		log.Printf("warning: could not load 404 template: %v", err)
 	}
 }
 
@@ -46,16 +50,14 @@ func main() {
 	if *index == "" {
 		log.Fatal("must specify index location")
 	}
-	if *itemplate == "" {
+	if *templates == "" {
 		log.Fatal("must specify index template location")
-	}
-	if *ptemplate == "" {
-		log.Fatal("must specify page template location")
 	}
 
 	RegisterSignalHandlers()
 	LoadTemplates()
 	BuildCache()
 
-	log.Fatal(http.ListenAndServe("127.0.0.1:8080", NewMux(*commondir)))
+	log.Printf("binding to %s", *bindAddr)
+	log.Fatal(http.ListenAndServe(*bindAddr, NewMux(*static)))
 }
